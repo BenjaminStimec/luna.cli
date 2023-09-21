a224a38702e408e930baf52955665b762e8c7da0
BenjaminStimec <74742450+BenjaminStimec@users.noreply.github.com>
2023-09-22 00:48:31 +0200

    More robust implementation using pyparsing

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import json
-import importlib
-from collections import defaultdict
 import os
+import parser
+import execute
 
-
-def load_mission(filename):
-    with open(filename, 'r') as f:
-        mission = json.load(f)
-    return mission
+LOGO_TEXT = """
+  ___     ___ ___ ______  _______    _______ ___     ___
+ |   |   |   Y   |   _  \|   _   |  |   _   |   |   |   |
+ |.  |   |.  |   |.  |   |.  1   |__|.  1___|.  |   |.  |
+ |.  |___|.  |   |.  |   |.  _   |__|.  |___|.  |___|.  |
+ |:  1   |:  1   |:  |   |:  |   |  |:  1   |:  1   |:  |
+ |::.. . |::.. . |::.|   |::.|:. |  |::.. . |::.. . |::.|
+ `-------`-------`--- ---`--- ---'  `-------`-------`---'
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ """
 
 def load_all_missions(operation):
     missions = []
@@ -22,70 +27,25 @@ def load_all_missions(operation):
             mission.append(mission)
     return missions
 
-def handle_variable_assignment(variable,value, vars):
-    vars.update({variable[1:]:value})
-
-def handle_variable_substitution(arg, vars):
-    return vars.get(arg[1:])
-
-
 def load_operation(filename):
     with open(filename, 'r') as f:
         operation = json.load(f)
     return operation
 
-def process_arguments(args_str, last_output, vars):
-    parsed_args = []
-
-    for arg in args_str.split(","):
-        if arg.startswith("$"):
-            parsed_args.append(handle_variable_substitution(arg, vars))
-        elif arg.startswith("%") and last_output != None:
-            if(type(last_output) is tuple):
-                parsed_args.append(last_output[arg[1:]])
-            else:
-                parsed_args.append(last_output)
-        else:
-            parsed_args.append(arg)
-    return parsed_args
-
-
-def execute_workflow(workflow, kit_folder, vars):
-    for step in workflow:
-        segments = step.split("=>")
-        last_output = None
-        for i, segment in enumerate(segments):
-            if segment.startswith("$") and last_output != None:
-                handle_variable_assignment(segment, last_output, vars)
-                continue
-            if(segment.startswith("[")):
-                continue
-            elif(segment.startswith("await")):
-                continue
-            func_str, *args_str = segment.split("(")
-            args_str = "".join(args_str)[:-1]  # Remove closing parenthesis and reassemble
-
-            # Split function strings into kit, module, and function
-            kit, module, func = func_str.split(".")
-
-            parsed_args = process_arguments(args_str, last_output, vars)
-            # Import module and function
-            imported_module = importlib.import_module(f"{kit_folder}.{kit}.{module}")
-            func_to_call = getattr(imported_module, func)
-
-            # Call function
-            result = func_to_call(*parsed_args)
-            last_output = result
-            print(result)
-
-import functions
+def execute_workflow(workflow, kits, vars):
+    parsed_workflow = parser.workflow.parseString(workflow)
+    execute.execute_parsed_workflow(parsed_workflow, kits, vars)
 
 if __name__ == "__main__":
+    # TODO alias functions - new section in mission files - allow kit.module.function names to be shortened to just 1 word
+    print(LOGO_TEXT)
     operation = load_operation("operation.json")
     print("starting operation: " + operation['name'])
     missions = load_all_missions(operation)
     for mission in missions:
         vars = mission.get("vars", {})
-        workflow = mission["workflow"]
+        workflows = mission["workflows"]
         print("executing mission: " + mission['name'])
-        functions.execute_workflow(workflow, operation['kit_folder'], vars)
+        for n, workflow in enumerate(workflows):
+            print("executing workflow number: " + str(n+1))
+            execute_workflow(workflow, operation['kit_folder'], vars)
