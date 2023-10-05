cecbe88583ac0fd94c245816519abf4662a1ffd2
BenjaminStimec <74742450+BenjaminStimec@users.noreply.github.com>
2023-10-05 03:56:12 +0200

    Improved notation for accessing the index of variables and tokens

--- execute.py
+++ execute.py
@@ -1,6 +1,91 @@
 import importlib
+import json
 import pyparsing
-from parser_types import LiteralString, VariableAssignment, Variable, DataStream, DefaultArg, PreviousOutput
+from lxml import etree
+from parser_types import LiteralString, VariableAssignment, Variable, DataStream, DefaultArg, Token
+
+from jsonpath_ng import jsonpath, parse
+
+def handle_json(data, indexing):
+    data_to_parse = ""
+    if isinstance(data, str):
+        try:
+            data_to_parse = json.loads(data)
+        except Exception as e:
+            raise ValueError(f'Unsuccessfully tried to convert string to json: {str(data)}, Error: {str(e)}')
+    elif isinstance(data, dict):
+        data_to_parse = data
+    else:
+        raise ValueError(f'Unviable data type for use with JSONPath: {str(data)}')
+    try:
+        jsonpath_expression = parse(indexing)
+    except Exception as e:
+        raise ValueError(f'Incorrect JSONPath expression: {str(indexing)}, Error: {str(e)}')
+    try:
+        matches = jsonpath_expression.find(data_to_parse)
+        if len(matches) == 1:
+            return matches[0].value
+        else:
+            return [match.value for match in matches]
+    except Exception as e:
+        raise ValueError(f'Failed JSONPath parsing: {str(data) + str(indexing)}, Error: {str(e)}')
+    
+
+def handle_xml(data, indexing):
+    try:
+        tree = etree.fromstring(data)
+    except Exception as e:
+        raise ValueError(f'Unsuccessfully tried to convert string to XML tree: {str(data)}, Error: {str(e)}')
+    try:
+        matches = tree.xpath(indexing)
+        if len(matches) == 1:
+            return matches[0].text if isinstance(matches[0], etree._Element) else matches[0]
+        else:
+            return [match.text if isinstance(match, etree._Element) else match for match in matches]
+    except Exception as e:
+        raise ValueError(f'Failed XPath parsing: {str(data) + indexing}, Error: {str(e)}')
+
+
+def handle_list(data, indexing):
+    if not isinstance(data, list):
+        raise ValueError(f'Expected a list, but got: {type(data).__name__}')
+    
+    index_exprs = indexing.split(',')
+    result = []
+    for expr in index_exprs:
+        if '-' in expr:
+            try:
+                start, end = map(int, expr.split('-'))
+            except ValueError as e:
+                raise ValueError(f'Invalid range expression: {expr}, Error: {str(e)}')
+            if start >= len(data) or end >= len(data) or start > end:
+                raise ValueError(f'Invalid range: {expr}, List length: {len(data)}')
+
+            result.extend(data[start:end + 1])
+        else:
+            try:
+                index = int(expr)
+            except ValueError as e:
+                raise ValueError(f'Invalid index expression: {expr}, Error: {str(e)}')
+            if index >= len(data):
+                raise ValueError(f'Index out of range: {index}, List length: {len(data)}')
+
+            result.append(data[index])
+    return result
+
+def apply_indexing(data, indexing):
+    expr_type_prefix = indexing[0]
+    expr_string = ''.join(indexing[2:-1])
+    dispatch_dict = {
+        'J': handle_json,
+        'X': handle_xml,
+        'L': handle_list
+    }
+    try:
+        handler = dispatch_dict[expr_type_prefix]
+    except KeyError:
+        raise ValueError(f'Unknown expression type prefix: {expr_type_prefix}')
+    return handler(data, expr_string)
 
 def execute_parsed_workflow(parsed_workflow, kits, vars):
     last_output = None
@@ -13,15 +98,19 @@ def execute_parsed_workflow(parsed_workflow, kits, vars):
                 if isinstance(arg, LiteralString) or isinstance(arg, DefaultArg):
                     parsed_args.append(arg.content)
                 elif isinstance(arg, Variable):
-                    # TODO: EXPANDED SYNTAX FOR VARIABLE - $name[n] - member of list, $name - entire return, $name[1-5, 7] - ranges, $name['abcd']['efg'][1-9]
                     if arg.name in vars:
-                        parsed_args.append(vars[arg.name])
+                        data = vars[arg.name]
+                        if arg.indexing is not '':
+                            data = apply_indexing(data, arg.indexing)
+                        parsed_args.append(data)
                     else:
                         raise ValueError(f"Variable {arg.name} is not defined in the vars dictionary")
-                elif isinstance(arg, PreviousOutput):
-                    # TODO: EXPANDED SYNTAX FOR PREVIOUS OUTPUT - %n - member of list, %all - entire return, %[1-5, 7] - ranges, %['abcd']['efg'][1-9]
+                elif isinstance(arg, Token):
                     if(last_output != None):
-                        parsed_args.append(last_output)
+                        data = last_output
+                        if arg.indexing is not '':
+                            data = apply_indexing(data, arg.indexing)
+                        parsed_args.append(data)
                     else:
                         raise ValueError(f"Last output is not available")
                 else:
--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from string import printable
 from collections import namedtuple
 from pyparsing import *
-from parser_types import LiteralString, Variable, VariableAssignment, DataStream, DefaultArg, PreviousOutput
+from parser_types import LiteralString, Variable, VariableAssignment, DataStream, DefaultArg, Token
 
 LPAR = "("
 RPAR = ")"
@@ -17,19 +17,28 @@ identifier = Word(alphas, alphanums + "_")
 
 literal_string = quotedString.setParseAction(lambda t: LiteralString(t[0][1:-1]))
 default_arg = Combine(OneOrMore(Word(printable, excludeChars=",()") | White())).setParseAction(lambda t: DefaultArg(t[0]))
-variable = (Literal("$") + identifier).setParseAction(lambda t: Variable(t[1]))
+
+
+EXPR_PREFIX = oneOf('J X L')
+
+extended_expr = Group(
+    EXPR_PREFIX("expr_type") +
+    LBRACE + SkipTo(RBRACE) + RBRACE
+)
+ 
+variable = (Literal("$") + identifier("var_name") + Optional(Literal(":") + extended_expr("index"))).setParseAction(lambda t: Variable(t.var_name, t.index))
 data_stream = (Literal("@") + identifier + LPAR + Optional(delimitedList(literal_string | variable | default_arg)) + RPAR).setParseAction(lambda t: DataStream(t[1]))
-previous_output = (Literal("%") + default_arg).setParseAction(lambda t: PreviousOutput(t[1].content))
+token = (Literal("%") + Optional(extended_expr("path"))).setParseAction(lambda t: Token("%", t.path))
+
 variable_assignment = (
     Literal("$") + identifier("var_name") + LPAR + 
-    previous_output("output") + RPAR
+    token("output") + RPAR
 ).setParseAction(lambda t: VariableAssignment(t.var_name, t.output))
 
-# TODO: foreach - execute the rest of the workflow for each element of a list
 function_call = Group(
     identifier("kit") + DOT +
     identifier("module") + DOT +
     identifier("function") + LPAR +
-    Optional(delimitedList(literal_string | variable | data_stream | previous_output | default_arg))("arguments") + RPAR
+    Optional(delimitedList(literal_string | variable | data_stream | token | default_arg))("arguments") + RPAR
 )
 workflow = Group(delimitedList(function_call | variable_assignment, ARROW))("workflow")
